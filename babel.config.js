ed26b8c5f4b37e5bc194022ce7458b0305426498
Alejandro <aleangrodriguez123@gmail.com>
2021-09-09 23:42:52 -0500

    fix: refactor folder and eslint

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '^~(.+)': './src/\\1',
+          },
+        },
+      ]
+    ]
   };
 };
