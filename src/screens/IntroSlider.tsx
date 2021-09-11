7a1f7284e20e6783548da2c58bdbce7eb6548e05
Alejandro <aleangrodriguez123@gmail.com>
2021-09-11 08:58:53 -0500

    fix: change labels

--- src/screens/IntroSlider.tsx
+++ src/screens/IntroSlider.tsx
@@ -4,15 +4,20 @@ import AppIntroSlider from 'react-native-app-intro-slider';
 import { Slide } from 'src/components/Slide';
 import { SlideIProps } from 'src/interfaces';
 
+import { DONE_LABEL_SLIDER, NEXT_LABEL_SLIDER } from 'src/constants';
+
 import { slides } from './slides';
 
 export default function IntroSlider() : React.ReactElement {
-  const renderItem = ({ item, index } : SlideIProps) => 
+
+  const renderItem = ({ item } : SlideIProps) => 
     <Slide item={item} />;
 
   return <AppIntroSlider 
     renderItem={renderItem}
     data={slides}
+    doneLabel={DONE_LABEL_SLIDER}
+    nextLabel={NEXT_LABEL_SLIDER}
   />;
 
 }
