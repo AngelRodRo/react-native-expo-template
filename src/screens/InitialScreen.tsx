a21af65bc79bd5b70cc0ce29cb393629851d3956
Alejandro <aleangrodriguez123@gmail.com>
2021-09-17 10:24:08 -0500

    clean: remove screens

---
+++ src/screens/InitialScreen.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default function InitialScreen() {
+  return (
+    <Text style={{ color: 'white' }}>
+      Hello world!
+    </Text>
+  )
+}
